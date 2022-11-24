5c60e6170ffd37b6bae4317e4dd2086e5668a5d4
SwathiBollepally <114129484+SwathiBollepally@users.noreply.github.com>
2022-11-23 22:21:29 -0600

    updating db.php

--- db.php
+++ db.php
@@ -1,9 +1,15 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("127.0.0.1","root","","bloodmanagementsystem");
+    //$con = mysqli_connect("127.0.0.1","root","","bloodmanagementsystem");
     // Check connection
-    if (mysqli_connect_errno()){
-        echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    }
+    //if (mysqli_connect_errno()){
+    //    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    //}
+
+   // mysql://b85f87976e7070:beb30064@us-cdbr-east-06.cleardb.net/heroku_cdf4ba87cc66d9c?reconnect=true
+    $db_host = "us-cdbr-east-06.cleardb.net";
+    $db_user = "b85f87976e7070";
+    $db_pass = "beb30064";
+    $db_name = "heroku_cdf4ba87cc66d9c";
 ?>
